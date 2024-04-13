86994b816b01ad7858e737cc7791596d45ad1779
sebalp100 <sebalp100@gmail.com>
2024-04-13 18:52:30 -0300

    Add base files and assets

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-pblack">AIVid</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">GO to profile</Link>
+    </View>
+  );
+}
