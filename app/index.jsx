c255bb30417b3329817e9fecd4427fefdb51b284
sebalp100 <sebalp100@gmail.com>
2024-04-16 23:56:38 -0300

    Add readme

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-pblack">AIVid</Text>
       <StatusBar style="auto" />
-      <Link href="/home">Go to Home</Link>
+      <Link href="/home">Go Home</Link>
     </View>
   );
 }
