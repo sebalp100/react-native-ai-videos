e548fb6896763dc66f6a10be6cb09286146ae081
sebalp100 <sebalp100@gmail.com>
2024-04-17 22:44:12 -0300

    Add home page

---
+++ app/components/CustomButton.jsx
@@ -0,0 +1,14 @@
+import { TouchableOpacity, Text } from 'react-native';
+import React from 'react';
+
+const CustomButton = () => {
+  return (
+    <TouchableOpacity
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center`}
+    >
+      <Text className="text-primary font-psemibold text-lg">CustomButton</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from './components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-pblack">AIVid</Text>
-      <StatusBar style="auto" />
-      <Link href="/home">Go Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className=" min-h-[85vh] w-full justify-center items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{' '}
+              <Text className="text-secondary-200">AIVid</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation. Embark on a journey of limitless
+            exploration with AIVid
+          </Text>
+          <CustomButton></CustomButton>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
