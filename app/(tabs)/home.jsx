5b207f9deae12cdcde4ed91fd98967a20149aeff
sebalp100 <sebalp100@gmail.com>
2024-04-24 22:07:34 -0300

    Add search input and recent videos to home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,30 +1,82 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
-import React from 'react';
-import { signOut } from '../../lib/appwrite';
-import { useGlobalContext } from '../../context/GlobalProvider';
-import { router } from 'expo-router';
-import { icons } from '../../constants';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList, Image, RefreshControl, Text, View } from 'react-native';
+
+import { images } from '../../constants';
+import useAppwrite from '../../lib/useAppwrite';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
+import EmptyState from '../../components/EmptyState';
+import SearchInput from '../../components/SearchInput';
+import Trending from '../../components/Trending';
+import VideoCard from '../../components/VideoCard';
 
 const Home = () => {
-  const { user, setUser, setIsLogged } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
-  const logout = async () => {
-    await signOut();
-    setUser(null);
-    setIsLogged(false);
+  const [refreshing, setRefreshing] = useState(false);
 
-    router.replace('/sign-in');
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
   };
+
   return (
-    <View className="h-full items-center justify-center">
-      <Text>Home</Text>
-      <TouchableOpacity
-        onPress={logout}
-        className="flex w-full items-center mb-10"
-      >
-        <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" />
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pregular text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white"></Text>
+              </View>
+
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={latestPosts ?? []} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
